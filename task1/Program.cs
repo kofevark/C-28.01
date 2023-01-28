96f24deb5f73de6db50bd201e79007d7ac3ce022
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-28 14:09:39 +0800

    Added task1 and task3

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Проверка, является ли первое число квадратом второго
+Console.Clear();
+Console.Write("Введите, пожалуйста, число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите, пожалуйста, число b: ");
+int b = int.Parse(Console.ReadLine());
+if (b * b == a) Console.WriteLine("Ура, число a является квадратом числа b!");
+else Console.WriteLine("Число a не является квадратом числа b");
