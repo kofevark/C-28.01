65dedf26ba4c9053821920e95f401cb0a24a5085
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-28 17:52:22 +0800

    Added task5 and task7

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход число N, на выходе показывает все целые числа от -N до N
+Console.Clear();
+Console.Write("Введите, пожалуйста, число N: ");
+int N = int.Parse(Console.ReadLine());
+int negativeN = -N;
+while (negativeN <= N)
+{
+    Console.Write(negativeN + " ");
+    negativeN++;
+}
---
+++ task7/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа принимает на входе трехзначное число, на выходе показывает последнюю цифру этого числа
+Console.Clear();
+Console.Write("Введите, пожалуйста, трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number%10);
