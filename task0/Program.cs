9d1f9d75d0724237eec32defbd5011ffe58ddfbb
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-28 13:25:09 +0800

    Added .gitignore

--- task0/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Программа возведения числа в квадрат
-Console.Clear();
-Console.Write("Введите, пожалуйста, число: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Квадрат числа " + a + " равен " + a * a);
