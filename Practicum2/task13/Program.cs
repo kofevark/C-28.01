0aa5276389068c6b955139d0fdb223df7a8c6d90
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-31 20:50:11 +0800

    Added task13 and task14

---
+++ Practicum2/task13/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
---
+++ Practicum2/task14/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2 || num2 * num2 == num1) Console.WriteLine($"{num1}, {num2} -> да");
+else Console.WriteLine($"{num1}, {num2} -> нет");
