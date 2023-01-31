51bfc3e06f502e4b3e42df620fecbce3bc07c353
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-01-31 20:12:30 +0800

    Added task11 and task12

--- Practicum2/task0/Program.cs
+++ Practicum2/task0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+Console.Clear();
+int random = new Random().Next(10, 100);
+Console.WriteLine(random);
+int firstNumber = random / 10;
+int secondNumber = random % 10;
+if (firstNumber > secondNumber) Console.WriteLine(random + " -> " + firstNumber);
+else Console.WriteLine(random + " -> " + secondNumber);
---
+++ Practicum2/task12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа принимает на вход два числа и выводит, является ли второе число кратным первому. В противом случае выводится остаток от деления.
+// Например:
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0) Console.WriteLine($"{num1}, {num2} -> кратно");
+else Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {num1 % num2}");
