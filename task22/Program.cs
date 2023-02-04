7bd4c5463a4ee44eb1cd0417c5f80223374c1cc5
Andrei_Filimonov <filimonoff.a@gmail.com>
2023-02-05 00:28:09 +0800

    Added task22

---
+++ task22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+Console.Clear();
+Console.WriteLine();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+Console.Write($"{num} ->");
+while (count <= num)
+{
+    Console.Write($" {Math.Pow(count, 2)},");
+    count++;
+}
